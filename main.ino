1405409fed25d891e39882310b9d2fa95923bb73
Koschi-the-turtle <koshivf@gmail.com>
2025-12-31 17:48:22 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,130 +1,136 @@
 #define SOIL_PIN A0
 #define LIGHT_PIN A1
 #define SPEAKER_PIN 8
-
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-enum PlantState {
-    THIRSTY, 
-    OKAY,
-    DROWNING,
-    DARK
-};
 
+enum PlantState {
+  THIRSTY,
+  OKAY, 
+  DROWNING, 
+  DARK
+};
 
 PlantState currentState;
 PlantState previousState;
 
-#define DRY_THRESHOLD 300
-#define DROWNING_THRESHOLD 700
-#define DARK_THRESHOLD 300
+#define DRY_THRESHOLD 50
+#define DROWNING_THRESHOLD 500
+#define DARK_THRESHOLD 850
 
 LiquidCrystal_I2C lcd (0x27, 16, 2);
 
 void playHappy() {
-    tone(SPEAKER_PIN, 1200, 150);
+  tone (SPEAKER_PIN, 800, 100);
+  delay (170);
+  tone(SPEAKER_PIN, 1200, 100);
 }
+
 void playSad() {
-    tone(SPEAKER_PIN, 300, 300);
+  tone (SPEAKER_PIN, 50, 300);
 }
 void playAlarm() {
-    for (int i=0; i<3; i++) {
-        tone(SPEAKER_PIN, 800, 100);
-        delay(150);
-    }
+  for (int i=0; i<3; i++) {
+    tone(SPEAKER_PIN, 100, 100);
+    delay(150);
+  }
 }
 
 void showMessage(PlantState state) {
-    lcd.clear();
-    switch (state) {
-        case THIRSTY:
-            lcd.setCursor(0, 0);
-            lcd.print("HELP! I NEED WATER");
-            lcd.setCursor(0, 1);
-            lcd.print("(x_x)");
-            break;
-        case OKAY:
-            lcd.setCursor(0, 0);
-            lcd.print("Feeling good");
-            lcd.setCursor(0, 1);
-            lcd.print("(^-^)");
-            break;
-        case DROWNING:
-            lcd.setCursor(0, 0);
-            lcd.print("BRO STOP I'M");
-            lcd.setCursor(0, 1);
-            lcd.print("DROWNING! (x_x)");
-            break;
-        case DARK:
-            lcd.setCursor(0, 0);
-            lcd.print("WHERE DID THE");
-            lcd.setCursor(0, 1);
-            lcd.print("SUN GO ?! (*-*)");
-    }
+  lcd.clear();
+  switch (state) {
+    case THIRSTY:
+    lcd.setCursor(0, 0);
+    lcd.print(F("HELP! (X_X)"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("I NEED WATER!"));
+    break;
+    case OKAY:
+    lcd.setCursor(0, 0);
+    lcd.print(F("Feeling good"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("(^-^)"));
+    break;
+    case DROWNING:
+    lcd.setCursor(0, 0);
+    lcd.print(F("BRO STOP I'M"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("DROWNING! (X_X)"));
+    break;
+    case DARK:
+    lcd.setCursor(0, 0);
+    lcd.print(F("WHERE DID THE"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("SUN GO?! (*-*)"));
+    break;
+  }
 }
 
 PlantState determineState(int soilValue) {
-    if (soilValue <= DRY_THRESHOLD) return THIRSTY;
-    if (soilValue <= DROWNING_THRESHOLD) return OKAY;
-    return DROWNING;
+  if (soilValue <= DRY_THRESHOLD) return THIRSTY;
+  if (soilValue <= DROWNING_THRESHOLD) return OKAY;
+  return DROWNING;
 }
-PlantState determineLightState(int lightValue) {
-    if (lightValue <= DARK_THRESHOLD) return DARK;
-    return OKAY;
+
+PlantState determineLightState (int lightValue) {
+  if (lightValue <= DARK_THRESHOLD) return DARK;
+  return OKAY;
 }
 
 PlantState combineStates(PlantState water, PlantState light) {
-    if (water == THIRSTY)
-        return THIRSTY;
-    if (water == DROWNING)
-        return DROWNING;
-    if (light == DARK)
-        return DARK;
-    return OKAY;
-
+  if (water == THIRSTY)
+    return THIRSTY;
+  if (water == DROWNING)
+    return DROWNING;
+  if (light == DARK)
+    return DARK;
+  return OKAY;
 }
+
 void onStateChange() {
-    showMessage(currentState);
-    switch (currentState) {
-        case THIRSTY:
-        case DARK:
-            playSad();
-            break;
-        case OKAY:
-            playHappy();
-            break;
-        case DROWNING:
-            playAlarm();
-            break;
-    }
+  showMessage(currentState);
+  switch (currentState) {
+    case THIRSTY:
+    case DARK:
+      playSad();
+      break;
+    case OKAY:
+      playHappy();
+      break;
+    case DROWNING:
+      playAlarm();
+      break;
+  }
 }
 
 void setup() {
-    pinMode(SPEAKER_PIN, OUTPUT);
-    pinMode(SOIL_PIN ,INPUT);
-    pinMode(LIGHT_PIN, INPUT);
-    lcd.init();
-    lcd.blacklight();
-    currentState = OKAY;
-    previousState = OKAY;
-    showMessage(currentState);
+  pinMode (SPEAKER_PIN, OUTPUT);
+  pinMode(SOIL_PIN, INPUT);
+  pinMode(LIGHT_PIN, INPUT);
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  currentState = OKAY;
+  previousState = OKAY;
+
+  showMessage(currentState);
 }
 
+void loop () {
+  int soilValue = analogRead(SOIL_PIN);
+  int lightValue = analogRead(LIGHT_PIN);
 
+  PlantState waterState = determineState(soilValue);
+  PlantState lightState = determineLightState(lightValue);
 
-
-void loop() {
-    int soilValue = analogRead(SOIL_PIN);
-    int lightValue = analogRead(LIGHT_PIN);
-
-    PlantState waterState = determineState(soilValue);
-    PlantState lightState = determineLightState(lightValue);
+  previousState = currentState;
+  currentState = combineStates(waterState, lightState);
     
-    previousState = currentState;
-    currentState = combineStates(waterState, lightState);
-
-    if (currentState != previousState) {
-        onStateChange();
-    }
-    delay(500);
+  if (currentState != previousState) {
+    onStateChange();
+  }
+  delay(150);
 }
